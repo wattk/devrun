bd53f2c3172df329de36128f0b0e36c3112f7f27
wattk <watt.kwon@gmail.com>
2021-12-21 21:35:22 +0900

    이벤트 등록 기능 구현 - 혜진 12/21 2135

--- src/main/java/com/kh/devrun/admin/model/dao/AdminDaoImpl.java
+++ src/main/java/com/kh/devrun/admin/model/dao/AdminDaoImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Repository;
 import com.kh.devrun.category.model.vo.ProductChildCategory;
 import com.kh.devrun.product.Product;
 import com.kh.devrun.product.ProductCategory;
+import com.kh.devrun.promotion.model.vo.Promotion;
 
 @Repository
 public class AdminDaoImpl implements AdminDao {
@@ -31,4 +32,28 @@ public class AdminDaoImpl implements AdminDao {
 	public int insertProducCategory(ProductCategory productCategory) {
 		return session.insert("product.insertProductCategory",productCategory);
 	}
+
+	/**
+	 * 혜진 시작
+	 */
+	@Override
+	public List<Product> selectProductListByProductCode(String searchCode) {
+		return session.selectList("product.selectProductListByProductCode", searchCode);
+	}
+
+	@Override
+	public int insertPromotion(Promotion promotion) {
+		return session.insert("promotion.insertPromotion", promotion);
+	}
+
+	@Override
+	public int insertProductPromotion(List<Map<String, Object>> list) {
+		return session.insert("promotion.insertProductPromotion", list);
+	}
+	
+	
+	/**
+	 * 혜진 끝
+	 */
+	
 }
