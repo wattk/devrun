2162410694d7189070434b2338d0c0ef2f31903e
taeyeongmin <alsxodud123@naver.com>
2021-12-21 11:12:57 +0900

    adminController 수정 상품추가 작업

--- src/main/java/com/kh/devrun/admin/model/dao/AdminDao.java
+++ src/main/java/com/kh/devrun/admin/model/dao/AdminDao.java
@@ -1,12 +1,15 @@
-package com.kh.devrun.category.model.service;
+package com.kh.devrun.admin.model.dao;
 
 import java.util.List;
 import java.util.Map;
 
 import com.kh.devrun.category.model.vo.ProductChildCategory;
+import com.kh.devrun.product.Product;
 
-public interface CategoryService {
+public interface AdminDao {
 
 	List<ProductChildCategory> selectChildCategory(Map<String, Object> param);
+
+	int insertProduct(Product product);
 
 }
