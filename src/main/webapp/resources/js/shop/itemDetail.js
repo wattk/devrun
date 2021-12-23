5643a31455adc953b3e2895059c736e921e1e275
DarciKim <greendory.k@gmail.com>
2021-12-23 15:21:54 +0900

    css 수정 - 김다현 12/23 1521

--- src/main/webapp/resources/js/shop/itemDetail.js
+++ src/main/webapp/resources/js/shop/itemDetail.js
@@ -14,6 +14,28 @@ $reviewModal.hide();
 }
 /**리뷰사진확대모달 끝*/
 
+ /**파일 업로드 시 바로 이미지 보기 시작 **/
+
+
+ var openFile = function(event) { 
+	const $picR = $('#choosePicP');
+	const picA = '<img id="previewPhoto" src="">';
+	$picR.after(picA);
+
+	var input = event.target; var reader = new FileReader(); 
+	 
+ 	reader.onload = function(){ 
+ 		var dataURL = reader.result; 
+ 		var output = document.getElementById('previewPhoto'); 
+ 		output.src = dataURL; 
+ 		}; 
+ 		
+ 	reader.readAsDataURL(input.files[0]); 
+ 	};
+
+
+/**파일 업로드 시 바로 이미지 보기 끝 **/ 
+
 /*상품리뷰 취소시 다 날리게 시작*/
 function deleteReview() {
 	$('#reviewPic').val("");
