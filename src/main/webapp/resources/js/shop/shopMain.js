0dcd7b68525f79b49bbc60b6d0686c7de218a4cd
wattk <watt.kwon@gmail.com>
2022-01-11 13:46:45 +0900

    실시간 순위 오류 수정 - 혜진 01/11 1346

--- src/main/webapp/resources/js/shop/shopMain.js
+++ src/main/webapp/resources/js/shop/shopMain.js
@@ -43,19 +43,17 @@ $(document).ready(()=>{
 
    
    /*실시간 순위 시작*/
-   $(function() {
-       var count = $('#rank-list li').length;
-       var height = $('#rank-list li').height();
+   var count = $('#rank-list li').length;
+   var height = $('#rank-list li').height();
    
-       function step(index) {
-           $('#rank-list ol').delay(2000).animate({
-               top: -height * index,
-           }, 500, function() {
-               step((index + 1) % count);
-           });
-       }
-       step(1);
-   });
+   function step(index) {
+       $('#rank-list ol').delay(2000).animate({
+           top: -height * index,
+       }, 500, function() {
+           step((index + 1) % count);
+       });
+   }
+   step(1);
    
    /*실시간 순위 끝*/
    
